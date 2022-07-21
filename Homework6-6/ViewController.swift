fdd74f2b728642e40b6b76a28b2927b3736fb34e
WeiFang Chou <chou.wei.fang@gmail.com>
2022-07-21 20:14:07 +0800

    Init Code

--- Homework6-6/ViewController.swift
+++ Homework6-6/ViewController.swift
@@ -9,11 +9,27 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var image: UIImageView!
+    @IBOutlet weak var alphaSlider: UISlider!
+    @IBOutlet weak var blueSlider: UISlider!
+    @IBOutlet weak var greenSlider: UISlider!
+    @IBOutlet weak var redSlider: UISlider!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func sliderChange(_ sender: UISlider) {
+        
+        
+        image.backgroundColor = UIColor(
+            red: CGFloat(redSlider.value / 255),
+            green: CGFloat(greenSlider.value / 255),
+            blue: CGFloat(blueSlider.value / 255),
+            alpha: CGFloat(alphaSlider.value / 255)
+        )
+        
+    }
+    
 }
 

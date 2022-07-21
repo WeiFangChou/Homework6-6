2f74b5ba07e77c9b83ae47d05bb49aa42b25812e
WeiFang Chou <chou.wei.fang@gmail.com>
2022-07-22 05:40:34 +0800

    新增 Gesture 以及陣列判斷

--- Homework6-6/ViewController.swift
+++ Homework6-6/ViewController.swift
@@ -8,15 +8,20 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    var images = ["Pikachu","Rowlet","Lucario","Eevee","Charizard"]
+    var index = 0
     @IBOutlet weak var image: UIImageView!
     @IBOutlet weak var alphaSlider: UISlider!
     @IBOutlet weak var blueSlider: UISlider!
     @IBOutlet weak var greenSlider: UISlider!
     @IBOutlet weak var redSlider: UISlider!
+    
+    @IBOutlet weak var pageControl: UIPageControl!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        pageControl.numberOfPages = images.count
     }
 
     @IBAction func sliderChange(_ sender: UISlider) {
@@ -31,5 +36,34 @@ class ViewController: UIViewController {
         
     }
     
+    @IBAction func resetColorChange(_ sender: Any) {
+        
+        redSlider.value = 0
+        greenSlider.value = 0
+        blueSlider.value = 0
+        alphaSlider.value = 255
+        
+    }
+    
+    
+    @IBAction func swipeLeft(_ sender: UISwipeGestureRecognizer) {
+        if(sender.direction == .right){
+            index += 1
+        }else if (sender.direction == .left){
+            index -= 1
+            
+        }
+        if(index == images.count){
+            index = 0
+        }else if(index < 0){
+            index = images.count - 1
+        }
+        image.image = UIImage(named: images[index])
+        pageControl.currentPage = index
+        
+    }
+    
+    
+    
+    
 }
-
